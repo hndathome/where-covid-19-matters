6d175908beae953b1c2dde77d9c1fdb8880b6bc7
Huong Devecsery <hndathome@gmail.com>
2020-10-05 13:53:21 -0500

    remove code that showed me how to do client dynamic routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,6 @@ exports.onCreatePage = async ({ page, actions }) => {
     const { createPage } = actions
     console.log("Page - ", page.path)
 
-    if (page.path.match(/^\/app/)) {
-        page.matchPath = "/app/*"
-        createPage(page)
-    }
-
     if (page.path.match(/^\/details/)) {
         page.matchPath = "/details/*"
         createPage(page)
