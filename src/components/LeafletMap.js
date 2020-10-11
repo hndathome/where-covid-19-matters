f2a257da990a5e17493f9a162851cc8170d708b5
Huong Devecsery <hndathome@gmail.com>
2020-10-11 12:23:32 -0500

    fix leaflet markers on refresh

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import Leaflet from 'leaflet';
 
-// class LeafletMap extends React.Component {
 function LeafletMap(props) {
     const { position, zoom, markers, keyMap } = props;
+    Leaflet.Icon.Default.imagePath = '../node_modules/leaflet'
+
+    delete Leaflet.Icon.Default.prototype._getIconUrl;
+
+    Leaflet.Icon.Default.mergeOptions({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    });
     return (
         <Map center={position} zoom={zoom} key={keyMap}>
             <TileLayer
