ccb877685e235cc1b1b2e26f648793f8b41b6ff2
Huong Devecsery <hndathome@gmail.com>
2020-10-11 09:30:58 -0500

    add leaflet headers bc on page refresh from server it doesn't see it

--- src/templates/ZipCodeDetail.js
+++ src/templates/ZipCodeDetail.js
@@ -19,12 +19,12 @@ export const ZipCodeDetail = (props) => {
                 <meta charSet="utf-8" />
                 <title>{zipcode} Details</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+                {/* <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
                     integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
                     crossorigin="" />
                 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
                     integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-                    crossorigin=""></script>
+                    crossorigin=""></script> */}
             </Helmet>
             <Layout>
                 <main role="main">
